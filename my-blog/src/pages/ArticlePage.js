8684a61897a80525f63da1b00cf47d1f36d145d4
AdrianVoinia <voiniaadrian@yahoo.com>
2022-03-21 17:39:25 +0200

    adding blogs from a json file and UI to navigate between them

--- my-blog/src/pages/ArticlePage.js
+++ my-blog/src/pages/ArticlePage.js
@@ -1,7 +1,24 @@
+import { useParams } from "react-router-dom";
+import articlesList from "./article-content";
+
 
 const ArticlePage = () => {
+    const { name } = useParams();
+    const article = articlesList.find(article =>  article.name === name);
+
+    if(!article) {
+        return (
+            <h1>This articol does not exist.</h1>
+        )
+    }
+
     return (
-    <h1>First article</h1>
+        <>
+            <h1>{article.name}</h1>
+            {article.content.map((paragraph, key) => (<p key={key}>
+                {paragraph}
+            </p>))}
+        </>
     )
 };
 
--- my-blog/src/pages/ArticlesList.js
+++ my-blog/src/pages/ArticlesList.js
@@ -1,7 +1,18 @@
+import articles from "./article-content";
+import { Link } from "react-router-dom";
+
 
 const ArticleList = () => {
     return (
-    <h1>The articles:</h1>
+        <>
+            <h1>The articles:</h1>
+            {articles.map((article, key) => (
+                <Link className="article-list-item" key={key} to={`/article/${article.name}`}>
+                    <h3>{article.title}</h3>
+                    <p>{article.content[0].substring(0, 150)}...</p>
+                </Link>))
+            }
+        </>
     )
 };
 
